17cacd943985dbf6c796900140a3b487b5e7160c
wangfy <wangfy@fenbi.com>
2017-12-13 15:32:16 +0800

    MOD:format for text-template/text.go

--- text-template/text.go
+++ text-template/text.go
@@ -131,8 +131,11 @@ func example() {
     {{if .Attended}}
         It was a pleasure to see you at the wedding.
     {{else}}
-        It is a shame you couldn't make it to the wedding.{{end}}
-        {{with .Gift}} Thank you for the lovely {{.}}.
+        It is a shame you couldn't make it to the wedding.
+    {{end}}
+    
+    {{with .Gift}} 
+        Thank you for the lovely {{.}}. 
     {{end}}
     Best wishes,
     Josie
